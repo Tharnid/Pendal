56c7d0fe9952233fb4bff5c0d21d1d411fd1e3bc
Jeff Smith <meddlingdeveloper@gmail.com>
2021-01-31 21:36:49 +0000

    interesting stuff

---
+++ adv_set_ops.py
@@ -0,0 +1,26 @@
+# friends = {"Terrell", "Tharnid", "Babydoll", "Jenny"}
+# abroad = {"Jenny", "Terrell"}
+
+# local_friends = friends.difference(abroad)
+# abroad_friends = abroad.difference(friends)
+# # print(local_friends)
+# print(abroad)
+
+# schedule = {"Illionis", "Indiana", "Iowa", "Michigan", "Michigan State", "Fordham", "Wake Forest"}
+# non_conf = {"Fordham", "Wake Forest"}
+
+# print(schedule)
+# conference = schedule.difference(non_conf)
+# print(conference)
+
+# # use shell to run files
+# full_schedule = conference.union(non_conf) # unites two sets
+# print(full_schedule)
+
+art = {"Bob", "Jen", "Randy", "Jay"}
+science = {"Bob", "Randy", "Tharnid", "Terrell"}
+
+both = art.intersection(science) # intersection people contained in both sets
+print(both)
+
+# single tuple xxx = 15, or "whatever",
