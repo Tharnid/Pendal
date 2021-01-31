d91b32cd12978405c7a6d48c18804ec7ba6b2356
Meddling Developer <meddlingdeveloper@gmail.com>
2021-01-31 20:35:23 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+l = ["Terrell", "Tharnid", "Babydoll"] # list []...
+t = ("Terrell", "Tharnid", "Babydoll") # tuple ()...can't modify
+s = {"Terrell", "Tharnid", "Babydoll"} # set {}...no duplicates
+
+# lists and tuples keep order
+
+# print(l[0]) # subscript notation
+l.append("Jenny")
+print(l)
