7943f011a4266d954473bf175df10384748abac6
Jeff Smith <meddlingdeveloper@gmail.com>
2021-02-08 18:10:21 +0000

    done with loops

--- loops.py
+++ loops.py
@@ -21,7 +21,16 @@
 
 # for loop
 
-friends = ["Terrell", "Saldor", "Jenny", "Tharnid"]
+# friends = ["Terrell", "Saldor", "Jenny", "Tharnid"]
 
-for friend in friends:  # for friend in range(4)
-  print(f"{friend} is my friend!!!")
+# for friend in friends:  # for friend in range(4)
+#   print(f"{friend} is my friend!!!")
+
+grades = [35, 67, 98, 100, 100]
+total = sum(grades)
+amount = len(grades)
+
+#for grade in grades:
+  # total += grade # shorthand of total = total + grade
+
+print(total / amount)
--- main.py
+++ main.py
@@ -5,3 +5,11 @@
 # for friend in friends:  # for friend in range(4)
 #   print(f"{friend} is my friend!!!")
 
+grades = [35, 67, 98, 100, 100]
+total = 0
+amount = len(grades)
+
+for grade in grades:
+  total += grade # shorthand of total = total + grade
+
+print(total / amount)
